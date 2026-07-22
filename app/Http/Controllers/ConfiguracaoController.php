73425d091f34704ae28d8c07b65633bc5502e2a6
Gabriel Gomes <gabriel.gomes@outlook.com>
2026-07-22 15:23:12 -0300

    feat: add support for dynamic menu permissions in ConfiguracaoController

--- app/Http/Controllers/ConfiguracaoController.php
+++ app/Http/Controllers/ConfiguracaoController.php
@@ -15,6 +15,14 @@ class ConfiguracaoController extends Controller
         if (!isset($configs['permitir_cartoes_presente'])) {
             $configs['permitir_cartoes_presente'] = '1';
         }
+
+        if (!isset($configs['permissoes_menus'])) {
+            $configs['permissoes_menus'] = json_encode([
+                'user' => ['/venda', '/transacoes', '/configuracoes'],
+                'gerencia' => ['/venda', '/dashboard', '/backoffice', '/estoque', '/transacoes', '/configuracoes'],
+                'diretoria' => ['/venda', '/dashboard', '/backoffice', '/estoque', '/transacoes', '/perfil', '/configuracoes']
+            ]);
+        }
         
         return response()->json($configs);
     }
@@ -26,12 +34,17 @@ class ConfiguracaoController extends Controller
             'permitir_vouchers' => 'required|in:0,1',
             'permitir_cartoes_presente' => 'required|in:0,1',
             'valor_padrao_voucher' => 'required|numeric|min:0',
+            'permissoes_menus' => 'nullable|string',
         ]);
 
         BazarConfiguracao::setValor('permitir_venda_funcionarios', $request->permitir_venda_funcionarios);
         BazarConfiguracao::setValor('permitir_vouchers', $request->permitir_vouchers);
         BazarConfiguracao::setValor('permitir_cartoes_presente', $request->permitir_cartoes_presente);
         BazarConfiguracao::setValor('valor_padrao_voucher', $request->valor_padrao_voucher);
+        
+        if ($request->has('permissoes_menus')) {
+            BazarConfiguracao::setValor('permissoes_menus', $request->permissoes_menus);
+        }
 
         return response()->json(['message' => 'Configurações salvas com sucesso!']);
     }
